81385b70f346c7020be07bf1132e286cfdb33ff0
IvanAnish <kaputa3000@gmail.com>
2022-05-25 16:09:24 -0400

    msg

--- src/Testooo.java
+++ src/Testooo.java
@@ -1,7 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Testooo {
-	public static void main(String[]args) {
+	public static void main(String[]args) throws IOException {
 		System.out.println("hello");
+		
+		File file = new File("/Users/ivan/Desktop/exampleText.txt");
+		Scanner scan = new Scanner(file);
+	
+		
+		String fileContent = "";
+		while (scan.hasNextLine()) {
+			fileContent = fileContent.concat(scan.nextLine() + "\n");
+		}
+		
+		
+		FileWriter writer = new FileWriter("/Users/ivan/Desktop/newFile.txt");
+		writer.write(fileContent);
+		writer.close();
+		
 	}
 
 }
