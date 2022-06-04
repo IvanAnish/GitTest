1071b8dc903e95acd753d83fd67511192f523a7a
IvanAnish <kaputa3000@gmail.com>
2022-06-04 13:08:08 -0400

    msg

--- src/Testooo.java
+++ src/Testooo.java
@@ -1,3 +1,4 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -7,20 +8,38 @@ import java.util.Scanner;
 public class Testooo {
 	public static void main(String[]args) throws IOException {
 		System.out.println("hello");
+		String[] names = {"Frodo", "Gandalf", "Legolas"};
 		
+		//scanner
 		File file = new File("/Users/ivan/Desktop/exampleText.txt");
 		Scanner scan = new Scanner(file);
 	
-		
+		//string file content
 		String fileContent = "";
 		while (scan.hasNextLine()) {
 			fileContent = fileContent.concat(scan.nextLine() + "\n");
 		}
+		scan.close();
 		
-		
+		//fileWriter
 		FileWriter writer = new FileWriter("/Users/ivan/Desktop/newFile.txt");
 		writer.write(fileContent);
 		writer.close();
+		
+		
+		//BufferedWriter
+		BufferedWriter writer2 = new BufferedWriter(new FileWriter("/Users/ivan/Desktop/bufferedOutput.txt"));
+		writer2.write("this is buffered writer");
+		for(String name : names) {
+			writer2.write("\n" + name);
+		}
+		writer2.close();
+		
+		//BufferedReader
+		
+		
+		
+		
 		
 	}
 
